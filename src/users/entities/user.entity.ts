e6f5a9b66f826d34b94f9ff796ff97b6822feab9
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-12-25 09:35:50 +0330

    creating user model for arangodb

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Collection, ArangoDocument } from 'nest-arango';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Collection('Users')
+export class UserEntity extends ArangoDocument {
+  @ApiProperty({ description: 'user id', example: '1' })
+  user_id?: string;
+
+  @ApiProperty({ description: 'username', example: 'john_doe' })
+  username: string;
+
+  @ApiProperty({ description: 'password', example: 'john_doe' })
+  password: string;
+
+  @ApiProperty({ description: 'role', example: 'ADMIN' })
+  role: 'ADMIN' | 'SUB_ADMIN' | 'USER';
+}
