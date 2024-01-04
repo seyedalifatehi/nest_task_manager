75d74f04abb7b3ee3d975c81cee6b34677c57015
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-01-04 08:19:23 +0330

    adding swagger ui to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Inventory API')
+    .setDescription('This API is for a sample inventory')
+    .setVersion('3.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
