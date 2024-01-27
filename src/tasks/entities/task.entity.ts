83947dc37cfb603b6846a2b063748f0ef5e15b74
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-01-27 14:37:32 +0330

    in tasks entity: delete unwnted codes

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,7 +1,7 @@
 export class Task {}
 import { Collection, ArangoDocument } from 'nest-arango';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsBoolean, IsDateString, IsDate } from 'class-validator';
+import { IsNotEmpty, IsString, IsBoolean, IsDate } from 'class-validator';
 
 @Collection('Tasks')
 export class TaskEntity extends ArangoDocument {
@@ -26,22 +26,6 @@ export class TaskEntity extends ArangoDocument {
   @ApiProperty({ description: 'isCompleted', example: 'true' })
   isCompleted: boolean;
 
-  // @IsNotEmpty()
-  // @IsString()
-  // @ApiProperty({
-  //   description: 'the date that the task defined',
-  //   example: '1402-01-23',
-  // })
-  // defineDate: string;
-
-  // @IsNotEmpty()
-  // @IsString()
-  // @ApiProperty({
-  //   description: 'the time that the task defined',
-  //   example: '12:00',
-  // })
-  // defineTime: string;
-
   // define date and time
   @ApiProperty({
     description: 'the date that the task defined',
@@ -50,24 +34,6 @@ export class TaskEntity extends ArangoDocument {
   @IsDate()
   defineDate: Date;
 
-  // --------------------------------------------------------------------
-
-  // @IsNotEmpty()
-  // @IsString()
-  // @ApiProperty({
-  //   description: 'the deadline date of the task',
-  //   example: '1402-01-23',
-  // })
-  // deadlineDate: string;
-
-  // @IsNotEmpty()
-  // @IsString()
-  // @ApiProperty({
-  //   description: 'the deadline time of the task',
-  //   example: '12:00',
-  // })
-  // deadlineTime: string;
-
   // deadline date and time
   @ApiProperty({
     description: 'the deadline date of the task',
@@ -75,5 +41,4 @@ export class TaskEntity extends ArangoDocument {
   })
   @IsDate()
   deadlineDate: Date;
-  
 }
