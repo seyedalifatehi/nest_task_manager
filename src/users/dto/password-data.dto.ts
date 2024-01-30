2bd95e3e434e13989422a1464f6610e84535a3a2
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-01-30 14:46:10 +0330

    in user dto folder: delete comment on the top of login user dto and password data dto

--- src/users/dto/password-data.dto.ts
+++ src/users/dto/password-data.dto.ts
@@ -1,5 +1,3 @@
-// this dto doesnt have id because arangodb creates the id automatically after we post data
-// this dto is used for sending data to
 import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
