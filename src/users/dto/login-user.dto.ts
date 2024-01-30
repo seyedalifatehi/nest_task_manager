63f233f35d851eae74a6c987e25fa614cffcf3bd
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-01-30 12:17:50 +0330

    format dto documents

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,15 +1,21 @@
 // this dto doesnt have id because arangodb creates the id automatically after we post data
-// this dto is used for sending data to 
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+// this dto is used for sending data to
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginUserDto {
-    @IsEmail()
-    @IsNotEmpty()
-    @ApiProperty({ description: 'the email of user', example: 'example@example.com' })
-    email: string;
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'the email of user',
+    example: 'example@example.com',
+  })
+  email: string;
 
-    @ApiProperty({ description: 'the password of user', example: 'Password@1234' })
-    @IsString()
-    password: string;
+  @ApiProperty({
+    description: 'the password of user',
+    example: 'Password@1234',
+  })
+  @IsString()
+  password: string;
 }
