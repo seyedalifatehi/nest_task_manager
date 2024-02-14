2e6c0bd3528f44c83d6a094e9b7295bf11a1f564
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-02-14 10:05:51 +0330

    in auth2.service file: create validateUser function

--- src/auth2/auth2.service.ts
+++ src/auth2/auth2.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from 'src/users/dto/login-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class Auth2Service {}
+export class Auth2Service {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(loginedUser: LoginUserDto) {
+    const user = await this.usersService.findOneUserByEmail(loginedUser.email);
+    if (user && (await bcrypt.compare(loginedUser.password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+}
