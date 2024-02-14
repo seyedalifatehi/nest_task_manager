0b539efeac34f9d444047a054a4cb2f0fa82ab7d
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-02-14 10:40:48 +0330

    in auth2.module file: add UsersService to the providers and configure JwtModule

--- src/auth2/auth2.module.ts
+++ src/auth2/auth2.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { Auth2Service } from './auth2.service';
 import { Auth2Controller } from './auth2.controller';
+import { UsersService } from 'src/users/users.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [Auth2Service],
-  controllers: [Auth2Controller]
+  providers: [Auth2Service, UsersService, JwtService],
+  controllers: [Auth2Controller],
+  imports: [
+    JwtModule.register({
+      secret: process.env.jwt_secret,
+      signOptions: {
+        // expiresIn: '3600s',
+      },
+    }),
+  ],
 })
 export class Auth2Module {}
