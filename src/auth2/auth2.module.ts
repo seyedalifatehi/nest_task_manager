0a5ca153bcb166aebd45abaa1b1523f441b18a15
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-02-14 10:47:54 +0330

    create: local-strategy.ts file and add it to auth2.module providers

--- src/auth2/auth2.module.ts
+++ src/auth2/auth2.module.ts
@@ -3,9 +3,10 @@ import { Auth2Service } from './auth2.service';
 import { Auth2Controller } from './auth2.controller';
 import { UsersService } from 'src/users/users.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local-strategy';
 
 @Module({
-  providers: [Auth2Service, UsersService, JwtService],
+  providers: [Auth2Service, UsersService, JwtService, LocalStrategy],
   controllers: [Auth2Controller],
   imports: [
     JwtModule.register({
