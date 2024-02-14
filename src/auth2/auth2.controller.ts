4965796aed2b49572c229df116e8ec99b6102bcb
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-02-14 11:00:34 +0330

    in auth2.controller: create POST method for login user

--- src/auth2/auth2.controller.ts
+++ src/auth2/auth2.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+  forwardRef,
+} from '@nestjs/common';
+import { Auth2Service } from './auth2.service';
+import { UsersService } from 'src/users/users.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth2')
-export class Auth2Controller {}
+export class Auth2Controller {
+  constructor(
+    private auth2Srevice: Auth2Service,
+
+    @Inject(forwardRef(() => UsersService))
+    private usersService: UsersService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return await this.auth2Srevice.login(
+      await this.usersService.findOneUserById(req.user._id),
+    );
+  }
+}
