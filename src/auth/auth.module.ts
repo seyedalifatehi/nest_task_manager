e761207cc6216211f99593cfd94a7e5cc34f91fa
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-02-15 09:35:43 +0330

    in auth.module file: delete constant.ts file importation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ArangoModule } from 'nest-arango';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { AuthController } from './auth.controller';
 import { UserEntity } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
